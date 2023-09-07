fbadc57acbf3cac9ec9dfd98dca2d65508466403
eduardorgv <eduardsabb@outlook.com>
2023-09-07 08:06:04 -0600

    Estructura de carpetas y componentes

---
+++ src/features/cart/CartItem.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { formatCurrency } from "../../utils/helpers";
+
+export const CartItem = ({ item }) => {
+  const { pizzaId, name, quantity, totalPrice } = item;
+
+  return (
+    <li>
+      <p>
+        {quantity}&times; {name}
+      </p>
+      <div>
+        <p>{formatCurrency(totalPrice)}</p>
+      </div>
+    </li>
+  );
+};
---
+++ src/features/order/OrderItem.jsx
@@ -0,0 +1,14 @@
+export const OrderItem = ({ item, isLoadingIngredients, ingredients }) => {
+  const { quantity, name, totalPrice } = item;
+
+  return (
+    <li>
+      <div>
+        <p>
+          <span>{quantity}&times;</span> {name}
+        </p>
+        <p>{formatCurrency(totalPrice)}</p>
+      </div>
+    </li>
+  );
+};
