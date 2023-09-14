68610fa2ca136086b58fd03a22e6a30144b024e6
eduardorgv <eduardsabb@outlook.com>
2023-09-14 14:29:15 -0600

    Cart page

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom';
 import { LinkButton } from '../../ui/LinkButton';
 import { Button } from '../../ui/Button';
+import { CartItem } from './CartItem';
 
 const fakeCart = [
   {
@@ -9,6 +10,7 @@ const fakeCart = [
     quantity: 2,
     unitPrice: 16,
     totalPrice: 32,
+    key: 1,
   },
   {
     pizzaId: 6,
@@ -16,6 +18,7 @@ const fakeCart = [
     quantity: 1,
     unitPrice: 13,
     totalPrice: 13,
+    key: 2,
   },
   {
     pizzaId: 11,
@@ -23,6 +26,7 @@ const fakeCart = [
     quantity: 1,
     unitPrice: 15,
     totalPrice: 15,
+    key: 3,
   },
 ];
 
@@ -30,18 +34,22 @@ export const Cart = () => {
   const cart = fakeCart;
 
   return (
-    <div>
-      <LinkButton
-        to="/menu"
-      >
-        &larr; Back to menu
-      </LinkButton>
+    <div className="px-4 py-3">
+      <LinkButton to="/menu">&larr; Back to menu</LinkButton>
 
-      <h2>Your cart, %NAME%</h2>
+      <h2 className="mt-7 text-xl font-semibold">Your cart, %NAME%</h2>
 
-      <div>
-        <Button type='primary' to='/order/new'>Order pizzas</Button>
-        <LinkButton>Clear cart</LinkButton>
+      <ul className="mt-3 divide-y divide-stone-600 border-b border-stone-600">
+        {cart.map((item) => (
+          <CartItem key={item.key} item={item} />
+        ))}
+      </ul>
+
+      <div className="mt-7 flex items-center justify-between space-x-4">
+        <Button type="primary" to="/order/new">
+          Order pizzas
+        </Button>
+        <Button type="secondary">Clear cart</Button>
       </div>
     </div>
   );
--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -5,6 +5,7 @@ export const Button = ({ children, disabled = false, to, type }) => {
     'rounded-full bg-green-300 text-base text-zinc-800 transition-colors duration-300 hover:bg-green-400 disabled:cursor-not-allowed';
   const styles = {
     primary: base + ' px-4 py-2 md:px-6 md:py-4',
+    secondary: 'rounded-full border-2 uppercase border-stone-300 text-base text-zinc-300 transition-colors duration-300 hover:bg-red-400 disabled:cursor-not-allowed px-4 py-2.5 md:px-6 md:py-3.5',
     small: base + ' px-8 py-1 md:px-4 md:py-2 text-sm'
   }
 
